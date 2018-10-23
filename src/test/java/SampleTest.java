b80b80ef451ad12a9db097acac7283ecf98d14f7
Yusuke Ohashi <github@junkpiano.me>
2018-10-23 12:50:51 +0900

    Init

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SampleTest {
+
+    @Test
+    public void num() {
+        assertEquals(10, Sample.num());
+        assertEquals("it's not 10.", 10, Sample.num());
+    }
+}
